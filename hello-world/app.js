c74deec24a5f6cdee112769a954a714c3d1517b8
Ninjin Nerguidavaa <108375365+n-ninjin@users.noreply.github.com>
2023-03-06 17:54:00 -0500

    Update app.js

--- hello-world/app.js
+++ hello-world/app.js
@@ -31,17 +31,36 @@ exports.handler = async (event) => {
         // Log the incoming record
         console.log('Incoming record: ', record)
 
-        // Get the original text from the S3 object in the incoming event
-        const originalText = await s3.getObject({
-          Bucket: record.s3.bucket.name,
-          Key: record.s3.object.key // Set S3FileKey parameter from record
-        }).promise()
 
-        // Parse the original text as JSON
-        const jsonData = JSON.parse(originalText.Body.toString('utf-8'))
+        if (event.eventName === 'FileUploadTXT' && record.s3.object.key.endsWith('.txt')) {
+          // Text
+          // Log the incoming record
+          console.log('FileUploadTXT: ', event.eventName )
 
-        // Call the ddbLoader function to upload the JSON data to DynamoDB
-        await uploadJsonToDynamoDb(jsonData, record.s3.object.key) // Pass S3FileKey parameter to ddbLoader
+          // Get the original text from the S3 object in the incoming event
+          const originalText = await s3.getObject({
+            Bucket: record.s3.bucket.name,
+            Key: record.s3.object.key // Set S3FileKey parameter from record
+          }).promise()
+
+          // Call the uploadTextToDynamoDb function to upload the text data to DynamoDB
+          await uploadTextToDynamoDb(originalText.Body.toString('utf-8'), record.s3.object.key) // Pass S3FileKey parameter to uploadTextToDynamoDb
+        } else {
+          console.log('FileUpload JSON: ', event.eventName )
+          // JSON
+          // Get the original text from the S3 object in the incoming event
+          const originalText = await s3.getObject({
+            Bucket: record.s3.bucket.name,
+            Key: record.s3.object.key // Set S3FileKey parameter from record
+          }).promise()
+
+          // Parse the original text as JSON
+          const jsonData = JSON.parse(originalText.Body.toString('utf-8'))
+
+          // Call the ddbLoader function to upload the JSON data to DynamoDB
+          await uploadJsonToDynamoDb(jsonData, record.s3.object.key) // Pass S3FileKey parameter to ddbLoader
+
+        }
 
       } catch (err) {
         // Log any errors
@@ -105,6 +124,72 @@ const uploadJsonToDynamoDb = async (data, s3FileKey) => {
         console.log('Success: ', result)
       } catch (err) {
         console.error('Error message: ', err)
+      }
+    })
+  )
+}
+
+// Load text data to DynamoDB table
+const uploadTextToDynamoDb = async (data, s3FileKey) => {
+  // Split the data into lines
+  const lines = data.split(/\r?\n/)
+
+  // Separate the lines into batches for upload
+  let batches = []
+  const BATCH_SIZE = 30
+
+  while (lines.length > 0) {
+    batches.push(lines.splice(0, BATCH_SIZE))
+  }
+
+  // Log the number of batches
+  console.log(`Total batches: ${batches.length}`)
+
+  // Initialize the batch counter
+  let batchCount = 0
+
+  // Save each batch to DynamoDB
+  await Promise.all(
+    batches.map(async (item_data) => {
+
+      // Set up the params object for the DynamoDB call
+      const params = {
+        RequestItems: {}
+      }
+      params.RequestItems[dynamoDbTableName] = []
+  
+      // Loop through each item in the batch
+      for (let i = 0; i < item_data.length; i++) {
+        // Generate a unique identifier for the item
+        const id = uuidv4()
+
+        // Set up the DynamoDB item object
+        const item = {
+          id,
+          text: item_data[i],
+          s3FileKey
+        }
+
+        // Add the item to the batch
+        params.RequestItems[dynamoDbTableName].push({
+          PutRequest: {
+            Item: item
+          }
+        })
+      }
+
+      // Log the number of items in the batch
+      console.log(`Batch ${batchCount} contains ${params.RequestItems[dynamoDbTableName].length} items`)
+
+      // Increment the batch counter
+      batchCount++
+
+      // Make the DynamoDB call to save the batch
+      try {
+        await docClient.batchWrite(params).promise()
+        console.log(`Batch ${batchCount - 1} saved successfully`)
+      } catch (err) {
+        console.error(`Error saving batch ${batchCount - 1}: ${err}`)
       }
     })
   )
